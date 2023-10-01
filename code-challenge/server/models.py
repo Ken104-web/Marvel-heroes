906c375d2d2a24f8127264c4428af0d6376bc15d
KenCorp <kennethwarui35@gmail.com>
2023-10-01 11:09:36 +0300

    Created models

--- code-challenge/server/models.py
+++ code-challenge/server/models.py
@@ -2,9 +2,43 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+# add any models you may need. 
 class Hero(db.Model):
-    __tablename__ = 'hero'
+    __tablename__ = 'heroes'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    super_name = db.Column(db.String)
+    created_at = db.Column(db.DateTime,server_default=db.func.now())
+    updated_at = db.Column(db.DateTime,onupdate=db.func.now())
 
-# add any models you may need. 
+    # - A Hero has many Power`s through HeroPower
+    powers = db.relationship('Power', secondary = 'hero_powers', back_populates="heroes")
+
+
+class Power(db.Model):
+    __tablename__ = 'powers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    description = db.Column(db.String)
+    created_at = db.Column(db.DateTime,server_default=db.func.now())
+    updated_at = db.Column(db.DateTime,onupdate=db.func.now())
+
+    #  A Power has many Hero`s through HeroPower
+
+    heroes = db.relationship('Hero', secondary = "hero_powers", back_populates="powers")
+class HeroPower(db.Model):
+    __tablename__ = 'hero_powers'
+    id = db.Column(db.Integer, primary_key=True)
+    strength =db.Column(db.String)
+
+    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'))
+    powers_id = db.Column(db.Integer, db.ForeignKey('powers.id'))
+
+    created_at = db.Column(db.DateTime,server_default=db.func.now())
+    updated_at = db.Column(db.DateTime,onupdate=db.func.now())
+
+
+    
+
